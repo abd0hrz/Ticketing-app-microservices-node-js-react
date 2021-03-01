4de6ecff8586a76b3c43e42a8542bc2eae0af8a1
hrz abdellah <63080277+abd0hrz@users.noreply.github.com>
2021-03-01 20:12:20 +0100

    add client to back

---
+++ client/api/build-client.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default ({ req }) => {
+  if (typeof window === 'undefined') {
+    return axios.create({
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    return axios.create({
+      baseUrl: '/',
+    });
+  }
+};
